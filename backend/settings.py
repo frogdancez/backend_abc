9013c20c344ed58f2983c3c081da69e86058257a
anh.dinh4 <58550812+adinh26101@users.noreply.github.com>
2022-07-28 16:33:23 +0700

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -145,3 +145,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_WHITELIST = [
+    'http://127.0.0.1:8000',
+    '*',
+]
