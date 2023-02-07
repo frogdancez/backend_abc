b8616f2ba4cb926ce69044b04b9b58bbe76e71e6
adinh26101 <bylycun@gmail.com.com>
2023-02-07 10:58:42 +0700

    2

--- create_data.py
+++ create_data.py
@@ -1,13 +1,13 @@
 import os
 
-os.system('python manage.py makemigrations')
-os.system('python manage.py migrate')
+os.system('python3 manage.py makemigrations')
+os.system('python3 manage.py migrate')
 
-os.system('python manage.py loaddata dataring/Customer.json')
-os.system('python manage.py loaddata dataring/Employee.json')
-os.system('python manage.py loaddata dataring/Store.json')
-os.system('python manage.py loaddata dataring/Product.json')
-os.system('python manage.py loaddata dataring/Order.json')
-os.system('python manage.py loaddata dataring/OrderDetail.json')
+os.system('python3 manage.py loaddata dataring/Customer.json')
+os.system('python3 manage.py loaddata dataring/Employee.json')
+os.system('python3 manage.py loaddata dataring/Store.json')
+os.system('python3 manage.py loaddata dataring/Product.json')
+os.system('python3 manage.py loaddata dataring/Order.json')
+os.system('python3 manage.py loaddata dataring/OrderDetail.json')
 
-os.system("""python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('a', 'admin@example.com', 'a')" """)
+os.system("""python3 manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('a', 'admin@example.com', 'a')" """)
